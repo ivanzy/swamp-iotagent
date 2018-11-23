5381e9ff46fd254c478ad1b30449c3fab3e71d6b
ivanzy <idimitryz@gmail.com>
2018-11-23 09:35:37 +0100

    corrected docker features

--- config/initializers/database.js
+++ config/initializers/database.js
@@ -1,10 +1,15 @@
-const config = require('nconf');
-const mongoose = require('mongoose');
+const config = require("nconf");
+const mongoose = require("mongoose");
 
-module.exports = (callback) => {
-   //initializing database
-   console.log('connecting to database... \n');
-   mongoose.connect(`mongodb://${config.get('DB_ADDRESS')}:${config.get("DB_PORT")}/swamp`);
-   const db = mongoose.connection;
-   callback();
- };
+module.exports = callback => {
+  //initializing database
+  console.log("connecting to database... \n");
+  setTimeout(() => {
+    mongoose.connect(
+      `mongodb://${config.get("DB_ADDRESS")}:${config.get("DB_PORT")}/swamp`
+    );
+    const db = mongoose.connection;
+  }, 5000);
+
+  callback();
+};
--- param.js
+++ param.js
@@ -3,13 +3,16 @@ const subscriber = require("./mqtt/subscribe");
 
 module.exports.loadEntities = () => {
   //load all entities in entities
-  Entity.getEntity((err, msg) => {
-    if (err) throw err;
-    else {
-      module.exports.entities = msg;
-      console.log("Subscribing in all stored entities:");
-      for (entity of msg) subscriber.entityWatcher(entity);
-    }
-  });
+  setTimeout(() => {
+    Entity.getEntity((err, msg) => {
+      if (err) throw err;
+      else {
+        module.exports.entities = msg;
+        console.log("Subscribing in all stored entities:");
+        for (entity of msg) subscriber.entityWatcher(entity);
+      }
+    });
+  }, 2000);
+
 };
  
